c395bfb4358736c537f5df8e826fdb44e5067e03
anders462 <anders462@gmail.com>
2016-01-05 15:25:58 -0800

    added user-agent

--- app/api/reqheadapp.js
+++ app/api/reqheadapp.js
@@ -5,7 +5,7 @@ var userData = {ipaddress:null, language:null, software: null};
 var headers = req.headers;
 var userData = {};
 userData["accept-language"] = req.headers["accept-language"];
-userData["accept-language"] = req.headers["accept-language"];
+userData["user-agent"] = req.headers["user-agent"];
 userData["x-forwarded-for"] = req.headers["x-forwarded-for"];
 
 return callBack(null, userData);
