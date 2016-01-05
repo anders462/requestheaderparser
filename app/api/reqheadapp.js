bda09f36befb9d6e0d7b9d51cc930a9bc5a49928
anders462 <anders462@gmail.com>
2016-01-05 15:18:16 -0800

    non filtered version

--- app/api/reqheadapp.js
+++ app/api/reqheadapp.js
@@ -2,9 +2,12 @@
 module.exports = function (req, callBack) {
 
 var userData = {ipaddress:null, language:null, software: null};
+var headers = req.headers;
+var userData = {};
+userData["accept-language"] = req.headers["accept-language"];
+userData["accept-language"] = req.headers["accept-language"];
+userData["x-forwarded-for"] = req.headers["x-forwarded-for"];
 
-userData = req.headers;
-
- return callBack(null, JSON.stringify(userData));
+return callBack(null, userData);
 
 };
