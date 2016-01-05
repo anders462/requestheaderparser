0dced13146442a33b7785a504d14ee48435c8fa5
anders462 <anders462@gmail.com>
2016-01-05 15:29:57 -0800

    correct prop names

--- app/api/reqheadapp.js
+++ app/api/reqheadapp.js
@@ -4,10 +4,10 @@ module.exports = function (req, callBack) {
 var userData = {ipaddress:null, language:null, software: null};
 var headers = req.headers;
 var userData = {};
-userData["accept-language"] = req.headers["accept-language"];
-userData["user-agent"] = req.headers["user-agent"];
-userData["x-forwarded-for"] = req.headers["x-forwarded-for"];
+userData["language"] = req.headers["accept-language"];
+userData["software"] = req.headers["user-agent"];
+userData["ipaddress"] = req.headers["x-forwarded-for"];
 
-return callBack(null, userData);
+return callBack(null, JSON.stringify(userData));
 
 };
